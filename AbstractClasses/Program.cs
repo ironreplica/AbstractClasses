c40a33a8c5ed55d06d516517ada95118d0a8342d
ironreplica <trevorchilds8@gmail.com>
2024-11-05 09:38:46 -0700

    Add project files.

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+Main();
+void Main()
+{
+    Animal[] animals = { new Dog(), new Cat(), new Horse() };
+    foreach (Animal animal in animals)
+    {
+        animal.EatFood();
+    }
+}
+
+public abstract class Animal
+{
+    public abstract void EatFood();
+}
+public class Dog : Animal
+{
+    public override void EatFood()
+    {
+        Console.WriteLine("Im a dog and im eating food.");
+    }
+}
+public class Cat : Animal
+{
+    public override void EatFood()
+    {
+        Console.WriteLine("Im a Cat and im eating food.");
+    }
+}
+public class Horse : Animal
+{
+    public override void EatFood()
+    {
+        Console.WriteLine("Im a Horse and im eating food.");
+    }
+}
